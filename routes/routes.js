20dfd21bdb67f169a7c4c0ee787fdef7037f1bac
AhmedAmineBen <benharrouz@hotmail.com>
2025-02-23 20:03:10 +0000

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 
-const DATA_FILE = path.join(__dirname, '..', 'routes.json');
+const DATA_FILE = path.join(process.cwd(), 'routes.json');
 
 // Fonction pour charger les itinéraires depuis le fichier JSON
 function loadRoutes() {
